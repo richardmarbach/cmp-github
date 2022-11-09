9ac8fed9b74a0a544d9c5a0d8a8bfadcd15a58ef
Richard Marbach <rmarbach@gmail.com>
2022-11-09 13:27:40 +0100

    Remove prefix regex

--- lua/cmp-github/init.lua
+++ lua/cmp-github/init.lua
@@ -9,7 +9,6 @@ source.new = function()
     fetched = false,
     items = {},
   }
-  self.prefix_regex = [[\c\(#\|issue:\?\s\?\)$]]
   return self
 end
 
@@ -30,63 +29,54 @@ end
 
 ---@param params cmp.SourceCompletionApiParams
 ---@param callback fun(response: lsp.CompletionResponse|nil)
-function source:complete(params, callback)
-  local trigger_kind = params.completion_context.triggerKind
-  local line = params.context.cursor_before_line
-  if trigger_kind == 1 and not vim.regex(self.prefix_regex):match_str(line) then
-    callback(nil)
-    return
-  end
-
+function source:complete(_, callback)
   if self.cache.fetched then
     callback(self.cache.items)
   else
-    Job
-      :new({
-        command = "gh",
-        args = {
-          "issue",
-          "list",
-          "-a",
-          "@me",
-          "--json",
-          "number,title,body,url",
-        },
-        on_exit = function(j, return_value)
-          if return_value ~= 0 then
-            self.cache.fetched = true
-            vim.notify(string.format("[cmp-github] Failed to fetch github issues: %s", j:result()), vim.log.levels.WARN)
-            callback(nil)
-            return
-          end
-
-          local issues = vim.json.decode(j:result()[1])
-
-          local items = {}
-          for _, issue in ipairs(issues) do
-            local description = string.sub(issue.body, 0, 500)
-            description = string.gsub(description, "\r", "")
-            if string.len(issue.body) > 500 then
-              description = description .. "..."
-            end
-
-            local detail = string.format("[#%d] %s", issue.number, issue.title)
-
-            table.insert(items, {
-              label = detail,
-              detail = detail,
-              documentation = { kind = "markdown", value = description },
-              insertText = issue.url,
-              sortText = string.format("%06d", issue.number),
-            })
-          end
+    Job:new({
+      command = "gh",
+      args = {
+        "issue",
+        "list",
+        "-a",
+        "@me",
+        "--json",
+        "number,title,body,url",
+      },
+      on_exit = function(j, return_value)
+        if return_value ~= 0 then
           self.cache.fetched = true
-          self.cache.items = items
+          vim.notify(string.format("[cmp-github] Failed to fetch github issues: %s", j:result()), vim.log.levels.WARN)
+          callback(nil)
+          return
+        end
 
-          callback(items)
-        end,
-      })
-      :start()
+        local issues = vim.json.decode(j:result()[1])
+
+        local items = {}
+        for _, issue in ipairs(issues) do
+          local description = string.sub(issue.body, 0, 500)
+          description = string.gsub(description, "\r", "")
+          if string.len(issue.body) > 500 then
+            description = description .. "..."
+          end
+
+          local detail = string.format("[#%d] %s", issue.number, issue.title)
+
+          table.insert(items, {
+            label = detail,
+            detail = detail,
+            documentation = { kind = "markdown", value = description },
+            insertText = issue.url,
+            sortText = string.format("%06d", issue.number),
+          })
+        end
+        self.cache.fetched = true
+        self.cache.items = items
+
+        callback(items)
+      end,
+    }):start()
   end
 end
 
